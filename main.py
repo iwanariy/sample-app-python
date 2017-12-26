7ba3c130d33693ba1977487a0fbc95d5e21a0a52
iwanariy <iwanariy@google.com>
2017-12-26 14:11:50 +0900

    add return

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         msg = u""
-        # msg += u"Hostname: {0}".format(os.environ.get("HTTP_HOST", ""))
-        msg += u"INSTANCE_ID: {0}".format(os.environ.get("INSTANCE_ID", ""))
+        # msg += u"HTTP_HOST: {0}\n".format(os.environ.get("HTTP_HOST", ""))
+        msg += u"INSTANCE_ID: {0}"\n.format(os.environ.get("INSTANCE_ID", ""))
         self.response.write(msg)
 
 
